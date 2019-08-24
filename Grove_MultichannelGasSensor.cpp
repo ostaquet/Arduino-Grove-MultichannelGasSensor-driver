bfed535bc4ad87b586261c3b504fbae4745bee1c
Olivier Staquet <ostaquet@gmail.com>
2019-08-24 08:26:53 +0200

    Initial commit

---
+++ Grove_MultichannelGasSensor.cpp
@@ -0,0 +1,374 @@
+#include "Grove_MultichannelGasSensor.h"
+
+/**
+ * Constructor
+ * Nothing special, just keep track of the parameters
+ */
+MiCS6814Class::MiCS6814Class(TwoWire& wire) {
+  _wire = &wire;
+}
+
+/**
+ * Destructor
+ * Nothing to do
+ */
+MiCS6814Class::~MiCS6814Class() {
+  
+}
+
+/**
+ * Start the usage of the driver
+ * Return :
+ *  - RC 0 if OK
+ *  - RC 1 if problem during the transmission attempt
+ *  - RC 2 if the device attached at the slave address is not the device expected
+ *  - RC 4 if the device has a different version than v2 (only the v2 is supported)
+ */
+int MiCS6814Class::begin(uint8_t slaveAddress) {
+  // Start the Wire prototol
+  _slaveAddress = slaveAddress;
+  _wire->begin();
+
+  // Check the init with the led (10 attempts)
+  bool isReady = false;
+  for(int i = 0; i < 10 && !isReady; i++) {
+    isReady = ledOn() == 0;
+    delay(50);
+    ledOff();
+    delay(50);
+  }
+  
+  if(!isReady) {
+    return 1;
+  }
+
+  // Check the version (works only with v2 of the sensor)
+  if(getVersion() != 2) {
+    return 4;
+  }
+
+  // Turn off heater and LED
+  ledOff();
+  heaterOff();
+
+  // Get R0 values by channel
+  r0ByChannel[0] = readOnCommand(MICS6814_CMD_READ_EEPROM, MICS6814_EEPROM_ADDR_USER_ADC_CH0);
+  r0ByChannel[1] = readOnCommand(MICS6814_CMD_READ_EEPROM, MICS6814_EEPROM_ADDR_USER_ADC_CH1);
+  r0ByChannel[2] = readOnCommand(MICS6814_CMD_READ_EEPROM, MICS6814_EEPROM_ADDR_USER_ADC_CH2);
+
+  return 0;
+}
+
+/**
+ * End the usage of the driver
+ */
+void MiCS6814Class::end() {
+  // Cleanup before leaving
+  ledOff();
+  heaterOff();
+
+  // Close the Wire protocol
+  _wire->end();
+}
+
+/**
+ * Sample the data on the sensor
+ */
+void MiCS6814Class::sample() {
+  // Mark the device as working
+  ledOn();
+  delay(10);
+  // Be sure that the heater is off
+  heaterOff();
+
+  // Let the sensor cold down
+  delay(1000);
+
+  // Switch on the heater
+  heaterOn();
+  delay(10);
+
+  int cycleCount = 1;
+  int stableCycleCount = 0;
+  int prevValueByChannel[3];
+
+  // First reading
+  readRsValues();
+  prevValueByChannel[0] = rSByChannel[0];
+  prevValueByChannel[1] = rSByChannel[1];
+  prevValueByChannel[2] = rSByChannel[2];
+  delay(1000);
+
+  while(stableCycleCount < MICS6814_DEFAULT_STABLE_CYCLE) {
+    readRsValues();
+    
+    if((abs(prevValueByChannel[0] - rSByChannel[0]) > 2)
+      || (abs(prevValueByChannel[1] - rSByChannel[1]) > 2)
+      || (abs(prevValueByChannel[2] - rSByChannel[2]) > 2)) {
+        prevValueByChannel[0] = rSByChannel[0];
+        prevValueByChannel[1] = rSByChannel[1];
+        prevValueByChannel[2] = rSByChannel[2];
+    } else {
+      stableCycleCount++;
+    }
+    
+    cycleCount++;
+    delay(1000);
+  }
+
+  heaterOff();
+  delay(10);
+  ledOff();
+  delay(10);
+}
+
+void MiCS6814Class::warmup() {
+  ledOn();
+  delay(10);
+  heaterOn();
+  delay(10);
+  
+  // For 30 minutes, let the heater on and blink the led
+  for(int min = 29; min >= 0; min--) {
+     for(int sec = 59; sec >= 0; sec--) {
+        // Show the user that the warmup is in progress...
+        if((min * 60 + sec) % 2 == 0) {
+          ledOn();
+        } else {
+          ledOff();
+        }
+        delay(1000);
+     }
+  }
+
+  // Should be good now :-)
+  heaterOff();
+  delay(10);
+  ledOff();
+  delay(10);
+}
+
+void MiCS6814Class::calibrate() {
+  // Sample at usual
+  sample();
+
+  // Prepare the data for I2C bus
+  uint8_t tmp[6];
+  tmp[0] = rSByChannel[0] >> 8;
+  tmp[1] = rSByChannel[0] & 0xFF;
+  tmp[2] = rSByChannel[1] >> 8;
+  tmp[3] = rSByChannel[1] & 0xFF;
+  tmp[4] = rSByChannel[2] >> 8;
+  tmp[5] = rSByChannel[2] & 0xFF;
+  
+  // Store the values as R0 on the EEPROM
+  Wire.beginTransmission(_slaveAddress);
+  Wire.write(MICS6814_CMD_SET_R0);
+  for(int i = 0; i < 6; i++) {
+    Wire.write(tmp[i]);
+  }
+  Wire.endTransmission();
+
+  // Remember the values as R0 in the local instance
+  r0ByChannel[0] = rSByChannel[0];
+  r0ByChannel[1] = rSByChannel[1];
+  r0ByChannel[2] = rSByChannel[2];
+}
+
+/**
+ * Internal helper to read the Rs values
+ */
+void MiCS6814Class::readRsValues() {
+  rSByChannel[0] = readOnCommand(MICS6814_CMD_READ_ADC_CH0);
+  rSByChannel[1] = readOnCommand(MICS6814_CMD_READ_ADC_CH1);
+  rSByChannel[2] = readOnCommand(MICS6814_CMD_READ_ADC_CH2);
+}
+
+/**
+ * Do the math and return the concentration of a specific gas
+ * It works only when a sample has been done...
+ */
+float MiCS6814Class::get(GasType gasType) {
+  // Compute the ratio based on R0 and Rs
+  float ratio0 = (float)rSByChannel[0]/(float)r0ByChannel[0]*(1023.0-r0ByChannel[0])/(1023.0-rSByChannel[0]);
+  float ratio1 = (float)rSByChannel[1]/(float)r0ByChannel[1]*(1023.0-r0ByChannel[1])/(1023.0-rSByChannel[1]);
+  float ratio2 = (float)rSByChannel[2]/(float)r0ByChannel[2]*(1023.0-r0ByChannel[2])/(1023.0-rSByChannel[2]);
+
+  float c = 0;
+
+  switch(gasType)
+  {
+      case CO:
+          c = pow(ratio1, -1.179) * 4.385;
+          break;
+      case NO2:
+          c = pow(ratio2, 1.007) / 6.855;
+          break;
+      case NH3:
+          c = pow(ratio0, -1.67) / 1.47;
+          break;
+      case C3H8:
+          c = pow(ratio0, -2.518) * 570.164;
+          break;
+      case C4H10:
+          c = pow(ratio0, -2.138) * 398.107;
+          break;
+      case CH4:
+          c = pow(ratio1, -4.363) * 630.957;
+          break;
+      case H2:
+          c = pow(ratio1, -1.8) * 0.73;
+          break;
+      case C2H5OH:
+          c = pow(ratio1, -1.552)*1.622;
+          break;
+      default:
+          break;
+  }
+
+  return isnan(c)?-3:c;
+}
+
+/**
+ * Change the I2C slave address
+ */
+void MiCS6814Class::changeSlaveAddress(uint8_t newSlaveAddress) {
+  command(MICS6814_CMD_CHANGE_I2C_ADDR, newSlaveAddress);
+  _slaveAddress = newSlaveAddress;
+  delay(5);
+}
+
+/**
+ * Display configuration on Serial
+ */
+void MiCS6814Class::displayConfig() {
+  if(Serial.available()) {
+    Serial.print("I2C_ADDRESS = "); Serial.println(readOnCommand(MICS6814_CMD_READ_EEPROM, MICS6814_EEPROM_ADDR_I2C_ADDRESS));
+    Serial.print("FACTORY_ADC_CH0 = "); Serial.println(readOnCommand(MICS6814_CMD_READ_EEPROM, MICS6814_EEPROM_ADDR_FACTORY_ADC_CH0));
+    Serial.print("FACTORY_ADC_CH1 = "); Serial.println(readOnCommand(MICS6814_CMD_READ_EEPROM, MICS6814_EEPROM_ADDR_FACTORY_ADC_CH1));
+    Serial.print("FACTORY_ADC_CH2 = "); Serial.println(readOnCommand(MICS6814_CMD_READ_EEPROM, MICS6814_EEPROM_ADDR_FACTORY_ADC_CH2));
+    Serial.print("USER_ADC_CH0 = "); Serial.println(readOnCommand(MICS6814_CMD_READ_EEPROM, MICS6814_EEPROM_ADDR_USER_ADC_CH0));
+    Serial.print("USER_ADC_CH1 = "); Serial.println(readOnCommand(MICS6814_CMD_READ_EEPROM, MICS6814_EEPROM_ADDR_USER_ADC_CH1));
+    Serial.print("USER_ADC_CH2 = "); Serial.println(readOnCommand(MICS6814_CMD_READ_EEPROM, MICS6814_EEPROM_ADDR_USER_ADC_CH2));
+  }
+}
+
+/**
+ * Return the version of the Grove Multichannel Gas Sensor
+ * Return values :
+ *  - 0 = unable to determine the version
+ *  - 1 = version 1
+ *  - 2 = version 2
+ */
+int MiCS6814Class::getVersion() {
+  unsigned int value = readOnCommand(MICS6814_CMD_READ_EEPROM, MICS6814_EEPROM_ADDR_SET);
+  if(value == 0x466) {
+    return 2;
+  } else if(value == 0xFFFF) {
+    return 0;
+  } else {
+    return 1;
+  }
+}
+
+/**
+ * Switch the heater ON
+ * Return codes, same as writeRegister()
+ */
+int MiCS6814Class::heaterOn() {
+  return command(MICS6814_CMD_CONTROL_PWR, 0x01);
+}
+
+/**
+ * Switch the heater OFF
+ * Return codes, same as writeRegister()
+ */
+int MiCS6814Class::heaterOff() {
+  return command(MICS6814_CMD_CONTROL_PWR, 0x00);
+}
+
+/**
+ * Switch the LED ON
+ * Return codes, same as writeRegister()
+ */
+int MiCS6814Class::ledOn() {
+  return command(MICS6814_CMD_CONTROL_LED, 0x01);
+}
+
+/**
+ * Switch the LED OFF
+ * Return codes, same as writeRegister()
+ */
+int MiCS6814Class::ledOff() {
+  return command(MICS6814_CMD_CONTROL_LED, 0x00);
+}
+
+/**
+ * Read 2 bytes in return of a command sent (without parameter)
+ * Return the unsigned int or 0 by default
+ */
+unsigned int MiCS6814Class::readOnCommand(uint8_t command) {  
+  _wire->beginTransmission(_slaveAddress);
+  _wire->write(command);
+  _wire->endTransmission();
+
+  delay(5); // Let the time for the bus to react
+  _wire->requestFrom(_slaveAddress, (uint8_t) 2);   // Add cast to avoid compile warning ;-)
+
+  uint8_t raw[2];
+ 
+  for (size_t i = 0; i < 2; i++) {
+    raw[i] = _wire->read();
+  }
+
+  unsigned int data = 0;
+  data = raw[0];
+  data <<= 8;
+  data += raw[1];
+  return data;
+}
+
+/**
+ * Read 2 bytes in return of a command sent with a parameter
+ * Return the unsigned int or 0 by default
+ */
+unsigned int MiCS6814Class::readOnCommand(uint8_t command, uint8_t parameter) {  
+  _wire->beginTransmission(_slaveAddress);
+  _wire->write(command);
+  _wire->write(parameter);
+  _wire->endTransmission();
+
+  delay(5); // Let the time for the bus to react
+  _wire->requestFrom(_slaveAddress, (uint8_t) 2);   // Add cast to avoid compile warning ;-)
+
+  uint8_t raw[2];
+ 
+  for (size_t i = 0; i < 2; i++) {
+    raw[i] = _wire->read();
+  }
+
+  unsigned int data = 0;
+  data = raw[0];
+  data <<= 8;
+  data += raw[1];
+  return data;
+}
+
+/**
+ * Write the value in the register defined by address
+ * Return codes:
+ *  - 0 OK
+ *  - 4 Problem during the transmission
+ */
+int MiCS6814Class::command(uint8_t command, uint8_t parameter) {
+  _wire->beginTransmission(_slaveAddress);
+  _wire->write(command);
+  _wire->write(parameter);
+  if (_wire->endTransmission() != 0) {
+    return 4;
+  }
+  return 0;
+}
+
+// Define the default MiCS6814 object
+MiCS6814Class MiCS6814(Wire);
---
+++ Grove_MultichannelGasSensor.h
@@ -0,0 +1,80 @@
+#include <Arduino.h>
+#include <Wire.h>
+
+// Default parameters
+#define MICS6814_DEFAULT_I2C_ADDR               0x04    // I2C slave address to use if not set
+#define MICS6814_DEFAULT_STABLE_CYCLE           20      // Number of cycles with low deviation to consider
+                                                        // the sample as stable and reliable
+
+// I2C commands
+#define MICS6814_CMD_READ_ADC_CH0               0x01
+#define MICS6814_CMD_READ_ADC_CH1               0x02
+#define MICS6814_CMD_READ_ADC_CH2               0x03
+#define MICS6814_CMD_CHANGE_I2C_ADDR            0x05
+#define MICS6814_CMD_READ_EEPROM                0x06
+#define MICS6814_CMD_SET_R0                     0x07
+#define MICS6814_CMD_CONTROL_LED                0x0A
+#define MICS6814_CMD_CONTROL_PWR                0x0B
+
+// EEPROM addresses (configuration stored on the Grove Multichannel Gas Sensor)
+#define MICS6814_EEPROM_ADDR_SET                0x00
+#define MICS6814_EEPROM_ADDR_FACTORY_ADC_CH0    0x02      // Factory setting for R0
+#define MICS6814_EEPROM_ADDR_FACTORY_ADC_CH1    0x04      // Factory setting for R0
+#define MICS6814_EEPROM_ADDR_FACTORY_ADC_CH2    0x06      // Factory setting for R0
+#define MICS6814_EEPROM_ADDR_USER_ADC_CH0       0x08      // User setting for R0 (after calibration)
+#define MICS6814_EEPROM_ADDR_USER_ADC_CH1       0x0A      // User setting for R0 (after calibration)
+#define MICS6814_EEPROM_ADDR_USER_ADC_CH2       0x0C      // User setting for R0 (after calibration)
+#define MICS6814_EEPROM_ADDR_I2C_ADDRESS        0x14      // I2C address stored on the system
+
+enum GasType {CO, NO2, NH3, C3H8, C4H10, CH4, H2, C2H5OH};
+
+class MiCS6814Class {
+  public:
+    MiCS6814Class(TwoWire& wire);
+    virtual ~MiCS6814Class();
+
+    // Start the driver for I2C address slaveAddress
+    // By default: use the MICS6814_DEFAULT_I2C_ADDR (0x04)
+    int begin(uint8_t slaveAddress = MICS6814_DEFAULT_I2C_ADDR);
+    void end();
+
+    // Obtain the firmware version of the Grove Multichannel Gas Sensor
+    // Return 0 if unknown, 1 = v1 and 2 = v2
+    int getVersion();
+    
+    // Output the configuration on Serial
+    void displayConfig();
+    
+    // Change the I2C slave address to the newSlaveAddress
+    void changeSlaveAddress(uint8_t newSlaveAddress);
+
+    // Sample the data from the sensor
+    void sample();
+
+    // Get the gas concentration for a specific gasType
+    // (data coming from the last call of sample())
+    float get(GasType gasType);
+
+    // Calibrate the sensor
+    void warmup();
+    void calibrate();
+
+  private:
+    // Internal helpers
+    unsigned int readOnCommand(uint8_t command);
+    unsigned int readOnCommand(uint8_t command, uint8_t parameter);
+    int command(uint8_t command, uint8_t parameter);
+    void readRsValues();
+    int ledOn();
+    int ledOff();
+    int heaterOn();
+    int heaterOff();
+
+    // Internal variables
+    TwoWire* _wire;
+    uint8_t _slaveAddress;
+    int r0ByChannel[3] = {0, 0, 0};
+    int rSByChannel[3] = {0, 0, 0};
+};
+
+extern MiCS6814Class MiCS6814;
